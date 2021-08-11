ecaf87ff6fb18282b3ae5df4904dd698d99d81a1
Rijin <rijinraju856@gmail.com>
2021-08-11 06:58:51 +0530

    first commit for react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import element from './moshreact/pro1';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <element/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/moshreact/pro1.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from '../App';
+const element = <h1>hello world</h1>
+ReactDom.render(element,document.getElementById('root'));
